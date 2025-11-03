7ccb2aa88d0924fbac7fc9570b8f72097cc9a97d
Satvik Mishra <satvikmishra2192@gmail.com>
2025-11-03 11:39:14 +0530

    Changed the CORS Config

--- src/main/java/com/learning/springboot/expensetrackerservice/Config/CorsConfig.java
+++ src/main/java/com/learning/springboot/expensetrackerservice/Config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:1234", "chrome-extension://*")
+                        .allowedOrigins("http://localhost:5173/", "chrome-extension://*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
